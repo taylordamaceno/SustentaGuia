b72c160482ff8090d281e3caac9c1a54b96195d8
taylordamaceno <taylordamaceno@gmail.com>
2023-10-23 16:50:36 -0300

    ajustes app SustentaGuia

---
+++ sustentaguia-react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
