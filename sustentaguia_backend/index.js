8f770673869bed966c080af0034c112a862947c8
taylordamaceno <taylordamaceno@gmail.com>
2023-10-24 15:56:19 -0300

    ajustes jwt

--- sustentaguia_backend/index.js
+++ sustentaguia_backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const cors = require('cors'); // Importar o CORS
+const cors = require('cors');
 const db = require('./database');
 
 const app = express();
@@ -9,18 +9,30 @@ const PORT = 5000;
 const SECRET_KEY = "90b7f602d68080c1571d9370b8b8683210595d2bc05677fabedaa61c78fa2a7d";
 
 app.use(express.json());
-app.use(cors()); // Adicionar o middleware CORS
+app.use(cors());
+
+// Função de middleware para autenticar token JWT
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
 
 app.post('/register', async (req, res) => {
     try {
         const { name, email, password } = req.body;
-
         const hashedPassword = await bcrypt.hash(password, 10);
         await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hashedPassword]);
-
         res.status(201).send({ message: 'User registered successfully!' });
     } catch (error) {
-        console.error("Erro ao registrar usuário:", error); // Log detalhado no backend
+        console.error("Erro ao registrar usuário:", error);
         res.status(500).send({ error: error.message });
     }
 });
@@ -28,7 +40,6 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-
         const user = (await db.query('SELECT * FROM users WHERE email = $1', [email])).rows[0];
         if (!user) return res.status(404).send({ error: 'User not found' });
 
@@ -38,12 +49,16 @@ app.post('/login', async (req, res) => {
         const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: '1h' });
         res.send({ message: 'Logged in successfully!', token });
     } catch (error) {
-        console.error("Erro ao logar usuário:", error); // Log detalhado no backend
+        console.error("Erro ao logar usuário:", error);
         res.status(500).send({ error: error.message });
     }
+});
+
+// Exemplo de rota protegida
+app.get('/dashboard', authenticateToken, (req, res) => {
+    res.send({ message: 'Bem-vindo ao dashboard!' });
 });
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
-
