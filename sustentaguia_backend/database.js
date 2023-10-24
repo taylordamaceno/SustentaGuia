989b99c27d243c2c43b464b80ec0c853115addae
taylordamaceno <taylordamaceno@gmail.com>
2023-10-24 10:43:51 -0300

    ajustes backend e frontend

--- sustentaguia_backend/database.js
+++ sustentaguia_backend/database.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'sustenguiadb',
-    password: 'SustentaGuia@2023',
+    database: 'sustentaguiadb',
+    password: 'SustentaGuia@2024',
     port: 5432,
 });
 
