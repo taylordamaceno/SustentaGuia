1ec05305f5c9ea29fed392f28c7a0b9997fc2fb4
taylordamaceno <taylordamaceno@gmail.com>
2023-10-25 15:26:10 -0300

    dashboard tela

--- sustentaguia-react/src/App.js
+++ sustentaguia-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import DashboardPage from './components/DashboardPage'; // Corrigindo o nome do import para DashboardPage
 import './styles/App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} /> {/* Rota para o DashboardPage */}
       </Routes>
     </Router>
   );
---
+++ sustentaguia-react/src/components/DashboardPage.js
@@ -0,0 +1,48 @@
+// DashboardPage.js
+
+import React, { useState, useEffect } from 'react';
+import dashboard from '../assets/images/dashboard.png';
+import '../styles/DashboardPage.css';
+
+function DashboardPage() {  // Corrigindo o nome da função
+  const [userProgress, setUserProgress] = useState({
+    modulesCompleted: 0,
+    quizzesPassed: 0
+  });
+
+  // Você pode substituir esse useEffect por uma chamada à sua API para obter o progresso real do usuário.
+  useEffect(() => {
+    // Exemplo: setUserProgress({ modulesCompleted: 3, quizzesPassed: 2 });
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <img src={dashboard} alt="Descrição da Imagem" className="dashboard-logo" />
+        <h1>Dashboard</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Módulos Introdutórios</h2>
+          <ul className="modules-list">
+            <li><i className="module-icon"></i> Módulo 1: Introdução à Energia Verde</li>
+            <li><i className="module-icon"></i> Módulo 2: Energia Solar</li>
+            <li><i className="module-icon"></i> Módulo 3: Energia Eólica</li>
+            <li><i className="module-icon"></i> Módulo 4: Energia Hidroelétrica</li>
+            <li><i className="module-icon"></i> Módulo 5: Biomassa e Biogás</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Progresso</h2>
+          <p>Módulos concluídos: {userProgress.modulesCompleted} de 5</p>
+          <p>Quizzes passados: {userProgress.quizzesPassed} de 5</p>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright © 2023</p>
+      </footer>
+    </div>
+  );
+}
+
+export default DashboardPage;  // Corrigindo o export para DashboardPage
