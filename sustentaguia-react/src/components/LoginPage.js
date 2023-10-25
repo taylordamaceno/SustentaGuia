f5b8e0101eda5ae253dbe4a718490ca1e2d31ce1
taylordamaceno <taylordamaceno@gmail.com>
2023-10-25 15:57:00 -0300

    ajustes corrigidos

--- sustentaguia-react/src/components/LoginPage.js
+++ sustentaguia-react/src/components/LoginPage.js
@@ -1,3 +1,4 @@
+// LoginPage.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import logo from '../assets/images/sustentaguia.png';
@@ -9,6 +10,8 @@ function LoginPage() {
     email: '',
     senha: ''
   });
+  const [errorMessage, setErrorMessage] = useState(null);
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -32,21 +35,21 @@ function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', { // Adicione o endereço completo aqui
+      const response = await axios.post('http://localhost:5000/login', {
         email: formData.email,
         password: formData.senha
       });
       console.log(response.data);
-
-      // Salvar o token no local storage
       localStorage.setItem('token', response.data.token);
-
-      // Redirecionar para o dashboard ou página inicial
-      window.location.href = '/dashboard';  // ou a rota que você deseja
+      window.location.href = '/dashboard';
     } catch (error) {
       console.error("Erro ao logar:", error.response.data);
+      setErrorMessage("Usuário ou senha inválidos");
+
     }
   };
+
+  const [activeTab, setActiveTab] = useState('login');
 
   return (
     <div className="App">
@@ -54,39 +57,48 @@ function LoginPage() {
         <img src={logo} alt="Logotipo do aplicativo" />
         <h1>Cadastro / Login</h1>
       </header>
+      <div className="tabs">
+        <button onClick={() => setActiveTab('login')} className={activeTab === 'login' ? 'active' : ''}>Login</button>
+        <button onClick={() => setActiveTab('register')} className={activeTab === 'register' ? 'active' : ''}>Cadastro</button>
+      </div>
       <main>
-        <section>
-          <h2>Cadastro</h2>
-          <form onSubmit={handleRegister}>
-            <div className="form-group">
-              <label htmlFor="nome">Nome</label>
-              <input type="text" className="form-control" id="nome" name="nome" placeholder="Seu nome" value={formData.nome} onChange={handleInputChange} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">E-mail</label>
-              <input type="email" className="form-control" id="email" name="email" placeholder="Seu e-mail" value={formData.email} onChange={handleInputChange} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="senha">Senha</label>
-              <input type="password" className="form-control" id="senha" name="senha" placeholder="Sua senha" value={formData.senha} onChange={handleInputChange} />
-            </div>
-            <button type="submit" className="btn btn-primary">Cadastrar</button>
-          </form>
-        </section>
-        <section>
-          <h2>Login</h2>
-          <form onSubmit={handleLogin}>
-            <div className="form-group">
-              <label htmlFor="email">E-mail</label>
-              <input type="email" className="form-control" id="email" name="email" placeholder="Seu e-mail" value={formData.email} onChange={handleInputChange} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="senha">Senha</label>
-              <input type="password" className="form-control" id="senha" name="senha" placeholder="Sua senha" value={formData.senha} onChange={handleInputChange} />
-            </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-          </form>
-        </section>
+        {activeTab === 'register' && (
+          <section>
+            <h2>Cadastro</h2>
+            <form onSubmit={handleRegister}>
+              <div className="form-group">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" className="form-control" id="nome" name="nome" placeholder="Seu nome" value={formData.nome} onChange={handleInputChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" className="form-control" id="email" name="email" placeholder="Seu e-mail" value={formData.email} onChange={handleInputChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="senha">Senha</label>
+                <input type="password" className="form-control" id="senha" name="senha" placeholder="Sua senha" value={formData.senha} onChange={handleInputChange} />
+              </div>
+              <button type="submit" className="btn btn-primary">Cadastrar</button>
+            </form>
+          </section>
+        )}
+        {activeTab === 'login' && (
+          <section>
+            <h2>Login</h2>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <form onSubmit={handleLogin}>
+              <div className="form-group">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" className="form-control" id="email" name="email" placeholder="Seu e-mail" value={formData.email} onChange={handleInputChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="senha">Senha</label>
+                <input type="password" className="form-control" id="senha" name="senha" placeholder="Sua senha" value={formData.senha} onChange={handleInputChange} />
+              </div>
+              <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+          </section>
+        )}
       </main>
       <footer>
         <p>Copyright © 2023</p>
