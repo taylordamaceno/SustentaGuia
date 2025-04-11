a0a1a86d37e3f4cf74bc64d354b7dcbb60f973e4
taylordamaceno <taylordamaceno@gmail.com>
2025-04-11 10:29:15 -0300

    refactoring em andamento

--- sustentaguia_frontend/app/page.tsx
+++ sustentaguia_frontend/app/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import  from "../src/index"
+import dynamic from "next/dynamic"
+
+// Carrega o App só no cliente, evitando erro de 'document is not defined'
+const App = dynamic(() => import("../src/App"), { ssr: false })
 
 export default function SyntheticV0PageForDeployment() {
-  return < />
+  return <App />
 }
